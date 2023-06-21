60c2bbc1a371f783efb0c22f923ac1e1344ea84a
haiiro <107000063+hai1ro@users.noreply.github.com>
2023-06-21 15:41:01 -0400

    Delete KennyRoom_pri directory

--- KennyRoom_pri/pri_view/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from pri_view import views
-
-urlpatterns = [
-    path('KennyRoom/', views.index, name='KennyRoom'),
-    # Otros patrones de URL para tu aplicación
-]
